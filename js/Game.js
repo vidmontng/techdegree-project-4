1edf6dd712feaad7eae7ac3bee86d7a730d55ffa
Nat <64110681+vidmontng@users.noreply.github.com>
2024-04-21 13:24:05 -0400

    Defining initial arguments and methods

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this. missed = 0;
+        this.phrases = [
+            "Life is beautiful",
+            "Friends are the family we choose",
+            "Nothing is impossible",
+            "Live life to the fullest",
+            "Even miracles take a little time"
+        ]
+            
+        this.activePhrase = null
+    }
+
+    startGame() {
+
+
+    }
+
+    getRandomPhrase() {
+
+
+    }
+
+    handleInteraction() {
+
+
+    }
+
+    removeLife() {
+
+
+    }
+
+    checkForWin() {
+
+
+    }
+
+    gameOver() {
+
+
+    }
+
+
+}
