a0f18f1e284e74f636caf4a6c25682b551ddbe43
Nat <64110681+vidmontng@users.noreply.github.com>
2024-05-07 23:16:35 -0400

    Working on "keyup" event for virtual keyboard

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 const placeholder = document.querySelector('#phrase ul');
 const startGameButton = document.querySelector('#btn__reset');
 const overlay = document.getElementById('overlay');
-const keys = document.getElementsByTagName('button');
+const keys = document.querySelectorAll('.key');
 const keyboard = document.querySelector('#qwerty');
 const hearts = document.getElementsByTagName('img');
 const liveHearts = document.querySelectorAll('img[src="images/liveHeart.png"]');
@@ -41,45 +41,48 @@ startGameButton.addEventListener('click', () =>  {
 
 
  
-
-// keyboard.addEventListener('kewdown', (e) => {
-//     let key = e.target;
-
-//     if (key.tagName === 'BUTTON') {
-//         const keyValue = key.textContent;
-//         game.handleInteraction(key, keyValue);
-//     }
-// });
-
-
-['click', 'keydown'].forEach(event => {
-    keyboard.addEventListener(event, (e) => {
+['keydown', 'click'].forEach(event => {
+    document.addEventListener(event, (e) => {
         
-        for (let i=0; i < keys.length; i ++) {
-            keys[i] = e.target;
-            const letter = e.target.textContent;
-            if (event === 'click'){
-                console.log(e.target.textContent);
-            } else if (event === 'keydown' && letter === e.key){
-                console.log(e.target);
+        
+        
+        keys.forEach(button => {
+          
+          const letter = button.textContent;
+
+            if (event === 'keydown' && letter === e.key) {
+                 game.handleInteraction(button, letter);
+             
             }
-            // keys[i].disabled = true;
-            //     keys[i].classList.remove('key');
-            //     keys[i].classList.add('chosen');
-        }
-    })
+
+//             if (event === 'click' && e.target.className === 'key') {
+// console.log(e.target);            
+//            }
+
+            
+});
+
+
+    
+   })
+
+
+
+
 });
 
 
 
+       
+        
+//THIS WoRKS!!!
 // document.addEventListener('keydown', (event) => {
 //            for (let i=0; i < keys.length; i ++) {
 //              const key = keys[i];
 //              const letter = key.textContent;
 
 //                 if (letter === event.key) {
-//                     key.classList.remove('key');
-//                     key.classList.add('chosen');
+//                     game.handleInteraction(key, letter);
 
 //                 }
 //            }
